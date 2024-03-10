657ac1bb82ed5f5baecc3e980f06a17ad6b7f6d8
Luc Nagel <lucmnagel@gmail.com>
2024-03-10 18:24:42 +0100

    Delete app/api/uploadthing directory

--- app/api/uploadthing/route.ts
+++
@@ -1,8 +0,0 @@
-import { createNextRouteHandler } from "uploadthing/next";
-
-import { uploadThingFileRouter } from "./core";
-
-// Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
-  router: uploadThingFileRouter,
-});
