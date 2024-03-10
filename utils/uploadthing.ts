ef93e009a390e7c4a1a13b42f40e902c24185961
Luc Nagel <lucmnagel@gmail.com>
2024-03-10 18:24:21 +0100

    Delete utils/uploadthing.ts

--- utils/uploadthing.ts
+++
@@ -1,4 +0,0 @@
-import { uploadThingFileRouter } from "@/app/api/uploadthing/core";
-import { generateComponents } from "@uploadthing/react";
-
-export const { UploadButton } = generateComponents<uploadThingFileRouter>();
